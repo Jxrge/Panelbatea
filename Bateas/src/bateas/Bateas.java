6d6795a4cd3a9cb16f47229788b562f995a043a3
Marcoslopezsantorum <72596886+Marcoslopezsantorum@users.noreply.github.com>
2021-05-31 00:06:49 +0200

    Add files via upload

---
+++ Bateas/src/bateas/Bateas.java
@@ -0,0 +1,16 @@
+
+package bateas;
+
+import bateas.com.Marcos.David.cuerdas;
+
+
+public class Bateas {
+
+
+    public static void main(String[] args) {
+       cuerdas obx = new cuerdas();
+           
+       obx.setVisible(true);
+    }
+    
+}
